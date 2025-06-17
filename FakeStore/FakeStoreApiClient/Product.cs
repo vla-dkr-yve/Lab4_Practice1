6620f8bde25cb5397330b616865de7e68cd81443
vla-dkr-yve <vladkryve@gmail.com>
2025-06-17 10:19:13 +0200

    Finished tasks

---
+++ FakeStore/FakeStoreApiClient/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FakeStoreApiCLient;
+public class Product
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("price")]
+    public decimal Price { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("category")]
+    public string Category { get; set; }
+
+    [JsonPropertyName("image")]
+    public string Image { get; set; }
+}
--- FakeStore/FakeStoreApiClient/Program.cs
+++ FakeStore/FakeStoreApiClient/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, Programming Languages 1!");
+﻿using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using FakeStoreApiCLient;
+
+var httpClient = new HttpClient();
+httpClient.BaseAddress = new Uri("https://fakestoreapi.com/");
+
+
+//var product = await httpClient.GetFromJsonAsync<Product>("products/1");
+//
+//product.Price = 1000;
+//
+//var response = await httpClient.PutAsJsonAsync("products/1", product);
+
+var response = await httpClient.DeleteAsync("products/1");
+Console.WriteLine(response);
